9b518de1b593bc932167a4712a2b304ed4572442
antonio miranda <antsmiranda@gmail.com>
2023-09-11 09:49:14 +0100

    Fix build for production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const path = require('path')
- 
+const path = require("path");
+
 module.exports = {
+//   output: 'export',
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-}
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return "parabolas-argumentos";
+  },
+};
